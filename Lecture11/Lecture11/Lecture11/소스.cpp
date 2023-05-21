c4bda3a10e466a30c5a8cf19aa87f169d7a6302d
damsister <hzzang08@gmail.com>
2023-05-21 23:43:22 +0900

    Lecture11 과제

--- Lecture11/Lecture11/Lecture11/소스.cpp
+++ Lecture11/Lecture11/Lecture11/소스.cpp
@@ -1,35 +1,36 @@
-#include <iostream>
-#include <string>
-
-class Object {
-private:
-    std::string name;
-
-public:
-    Object(const std::string& name) : name(name) {}
-
-    std::string get_name() const {
-        return name;
-    }
-
-    void set_name(const std::string& new_name) {
-        name = new_name;
-    }
-
-    void display() const {
-        std::cout << "Object name: " << name << std::endl;
-    }
-};
-
-int main() {
-    Object obj("Example");
-    obj.display();
-
-    std::string new_name;
-    std::cout << "새로운 이름을 입력하세요: ";
-    std::getline(std::cin, new_name);
-    obj.set_name(new_name);
-    obj.display();
-
-    return 0;
-}
+////예제
+//#include <iostream>
+//#include <string>
+//
+//class Object {
+//private:
+//    std::string name;
+//
+//public:
+//    Object(const std::string& name) : name(name) {}
+//
+//    std::string get_name() const {
+//        return name;
+//    }
+//
+//    void set_name(const std::string& new_name) {
+//        name = new_name;
+//    }
+//
+//    void display() const {
+//        std::cout << "Object name: " << name << std::endl;
+//    }
+//};
+//
+//int main() {
+//    Object obj("Example");
+//    obj.display();
+//
+//    std::string new_name;
+//    std::cout << "새로운 이름을 입력하세요: ";
+//    std::getline(std::cin, new_name);
+//    obj.set_name(new_name);
+//    obj.display();
+//
+//    return 0;
+//}
